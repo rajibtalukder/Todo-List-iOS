55459fb9e826260a62d3198853ec8c8d7b8f6fa8
rajibtalukder <rajibtalukder392@gmail.com>
2026-04-22 01:17:48 +0600

    Initial Commit

---
+++ todo_list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo_list
+//
+//  Created by MacBook on 22/4/26.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
